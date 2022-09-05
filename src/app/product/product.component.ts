008d2a0a4ca5e2dfa8a7d4df08f60c1b218a8d49
huricaki <hurcak04@gmail.com>
2022-09-05 21:33:04 +0300

    ders 14-15

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,11 +10,17 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title="Ürün Listesi"
+  filterText= ""
   products: Product[]=[
     {id:1, name:"laptop", price:2500, categoryid:1, description:"Asus Zenbook", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
-    {id:2, name:"mouse", price:250, categoryid:1, description:"Asus", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"}
+    {id:2, name:"mouse", price:250, categoryid:1, description:"Asus", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
+    {id:3, name:"laptop", price:100, categoryid:1, description:"Asus Zenbook", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
+    {id:4, name:"mouse", price:2050, categoryid:1, description:"Asus", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"}
   ]
   ngOnInit(): void {
   }
 
+ addToCard(prdct :Product):void{
+  alert("sepete eklendi: "+prdct.name)
+ }
 }
--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,6 +1,6 @@
  export class Product{
     id:number | undefined;
-    name:string | undefined;
+    name?:string ;
     price: number | undefined;
     categoryid:number | undefined;
     description:string | undefined;
