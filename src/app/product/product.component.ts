80da77765b8ede11d904560eda82f7aa511f9c51
huricaki <hurcak04@gmail.com>
2022-11-05 21:49:29 +0300

    ders 16-17-18.1

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
+import { AlertifyService } from '../Services/alertify.service';
 
 @Component({
   selector: 'app-product',
@@ -8,19 +9,25 @@ import { Product } from './product';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertifyService :AlertifyService) { }
   title="Ürün Listesi"
   filterText= ""
   products: Product[]=[
-    {id:1, name:"laptop", price:2500, categoryid:1, description:"Asus Zenbook", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
-    {id:2, name:"mouse", price:250, categoryid:1, description:"Asus", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
-    {id:3, name:"laptop", price:100, categoryid:1, description:"Asus Zenbook", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
-    {id:4, name:"mouse", price:2050, categoryid:1, description:"Asus", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"}
+    {id:1, name:"Laptop", price:2500, categoryid:1, description:"Asus Zenbook", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
+    {id:2, name:"Mouse", price:250, categoryid:1, description:"Asus", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
+    {id:3, name:"Laptop", price:100, categoryid:1, description:"Asus Zenbook", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
+    {id:4, name:"Mouse", price:2050, categoryid:1, description:"Asus", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"}
   ]
   ngOnInit(): void {
   }
 
  addToCard(prdct :Product):void{
-  alert("sepete eklendi: "+prdct.name)
+  this.alertifyService.success(prdct.name +" Eklendi.")
+  //alert("sepete eklendi: "+prdct.name)
  }
 }
+
+
+
+
+
