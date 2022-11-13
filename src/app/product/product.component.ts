a35c93bb2875209feba985b6310834ed6be63643
huricaki <hurcak04@gmail.com>
2022-11-13 13:14:57 +0300

    19.1-19.2-19.3 ders commit

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { AlertifyService } from '../Services/alertify.service';
+import { ProductService } from '../Services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
+  providers:[ProductService]
 })
 export class ProductComponent implements OnInit {
 
-  constructor(private alertifyService :AlertifyService) { }
+  constructor(
+    private alertifyService :AlertifyService,
+    private productService:ProductService) { }
   title="Ürün Listesi"
   filterText= ""
-  products: Product[]=[
-    {id:1, name:"Laptop", price:2500, categoryid:1, description:"Asus Zenbook", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
-    {id:2, name:"Mouse", price:250, categoryid:1, description:"Asus", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
-    {id:3, name:"Laptop", price:100, categoryid:1, description:"Asus Zenbook", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"},
-    {id:4, name:"Mouse", price:2050, categoryid:1, description:"Asus", imageUrl:"https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"}
-  ]
-  ngOnInit(): void {
+  products!: Product[];
+ 
+  ngOnInit(){
+    this.productService.getProducts().subscribe(data=>
+      this.products=data
+      );
   }
 
  addToCard(prdct :Product):void{
